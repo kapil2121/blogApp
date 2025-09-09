902c118445a950a05222680bac92ccb2c95179d4
Kapil Kumar <kapilkumar10000@gmail.com>
2025-09-09 11:01:17 +0530

    JWT token authentication

--- blog/routers/user.py
+++ blog/routers/user.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from .. import schemas, database
+from .. import schemas, database, oauth2
 from ..repository import user
 
 router = APIRouter(
@@ -11,10 +11,10 @@ router = APIRouter(
 get_db = database.get_db
 
 @router.post('/', response_model=schemas.ShowUser)
-def create_user(request: schemas.User, db:Session = Depends(get_db)):
+def create_user(request: schemas.User, db:Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return user.create_user(request, db)
 
 
 @router.get('/{id}', status_code=200, response_model=schemas.ShowUser)
-def get_user(id:int, db: Session = Depends(get_db)):
+def get_user(id:int, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return user.get_user(id, db)
